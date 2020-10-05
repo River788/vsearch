c2d09e76eace69558e5216e990afe1571cd506e6
River788 <72371282+River788@users.noreply.github.com>
2020-10-05 15:28:14 +0900

    second. 2回目。
    Practice-python.
    search4letters関数が追加。

--- vsearch.py
+++ vsearch.py
@@ -1,7 +1,18 @@
+"""
+Practice-Python
+"""
 
-def search4vowels(word:str) -> set:
+
+def search4vowels(phrase:str) -> set:
     """指定された単語内の母音を返す"""
     vowels = set('aeiou')
-    return vowels.intersection(set(word))
+    return vowels.intersection(set(phrase))
+#PEP8 によると、トップレベル関数では、
+#2行の空行を開けることを推奨しています。
+def search4letters(phrase:str, letters:str,) -> set:
+    """phrase内のlettersの集合を返す。"""
+    return set(letters).intersection(set(phrase))
     
-    
+
+
+
